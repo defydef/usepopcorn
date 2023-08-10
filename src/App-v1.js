e56847db5df9b8155fe609602f0557ad1f8ca14a
defy <8841170+defydef@users.noreply.github.com>
2023-08-10 07:57:43 +0700

    Return App.js back to index.js

---
+++ src/App-v1.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { NavBar, NumResult, Search } from "./NavBar";
+import Main from "./Main";
+import MovieList from "./MainComponents/MovieList";
+import { MoviesBox, WatchedBox } from "./Main";
+
+export default function App() {
+  const tempMovieData = [
+    {
+      imdbID: "tt1375666",
+      Title: "Inception",
+      Year: "2010",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    },
+    {
+      imdbID: "tt0133093",
+      Title: "The Matrix",
+      Year: "1999",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+    },
+    {
+      imdbID: "tt6751668",
+      Title: "Parasite",
+      Year: "2019",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+    },
+  ];
+  const [movies, setMovies] = useState(tempMovieData);
+  return (
+    <>
+      <NavBar>
+        <Search />
+        <NumResult movies={movies} />
+      </NavBar>
+      <Main>
+        <MoviesBox>
+          <MovieList movies={movies} type="all"></MovieList>
+        </MoviesBox>
+        <WatchedBox />
+      </Main>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} color="red" size="48" />
+    <App />
+    {/* <StarRating maxRating={5} color="red" size="48" />
     <StarRating
       maxRating={5}
       size="52"
       messages={["Terrible", "Bad", "Okay", "Good", "Awesome"]}
     />
     <StarRating defaultRating={3} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
